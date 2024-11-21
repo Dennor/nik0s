28ee95949ffff42bd51ab2414abcc487124c3b26
Dennor <kamil.matysiewicz@pm.me>
2024-11-21 07:54:36 +0100

    chore: bump nixpkgs (#31)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Defines k0s cluster nodes configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = {
