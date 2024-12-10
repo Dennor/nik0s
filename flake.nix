42d8d39378cbfcabdd53ad46d1e20294a1da0ccc
Kamil Matysiewicz <kamil.matysiewicz@pm.me>
2024-12-10 14:24:32 +0100

    feat: bump cilium in soju-jinro

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Defines k0s cluster nodes configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
   outputs = {
